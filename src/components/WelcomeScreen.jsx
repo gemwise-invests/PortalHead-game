c57b15b527c2d4646b42f41cf581699f8079fc2c
Dr Lawrence Lau <14944510+drllau@users.noreply.github.com>
2021-11-09 10:37:47 +0800

    Update WelcomeScreen.jsx
    Changed startup text

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -5,13 +5,13 @@ export const WelcomeScreen = ({ onStartButtonClick }) => {
   return (
     <Box height="100vh" width="100vw" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
       <Text as="h1" fontSize="3xl" fontFamily="Lily Script One">
-        Flipping Cards
+        PortalHeads Demo
       </Text>
 
       <Box w="100%" h={10} />
 
       <Button variant="solid" variantColor="blue" size="lg" display="block" onClick={onStartButtonClick}>
-        Start Game
+        Start Demo
       </Button>
     </Box>
   );
